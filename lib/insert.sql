e9eff42ff7c6ec77d9a0068acb9c4e9496a34513
Joshua Vickstrom <joshuavickstrom@outlook.com>
2018-02-20 17:38:26 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -5,11 +5,11 @@ INSERT INTO books (title, year) VALUES ("The Hunger Games", 2008), ("Catching Fi
 INSERT INTO books (title, year) VALUES ("Across The Nightingale Floor", 2002), ("Grass For His Pillow", 2003), ("Brilliance Of The Moon", 2004);
 
 INSERT INTO characters (name, motto, species) VALUES ("Katnis", "I volunteer.", "human");
-INSERT INTO characters (name, motto, species) VALUES ("Peeta", "I love Katnis.", "human");
-INSERT INTO characters (name, motto, species) VALUES ("Takeo", "I am a ninja.", "human");
+INSERT INTO characters (name, motto, species) VALUES ("Peeta", "I love Katnis!", "human");
+INSERT INTO characters (name, motto, species) VALUES ("Gale", "I love Katnis, too!", "human");
+INSERT INTO characters (name, motto, species) VALUES ("Effie", "May the odds be...", "human");
 INSERT INTO characters (name, motto, species) VALUES ("Kaede", "I love Takeo.", "human");
-INSERT INTO characters (name, motto, species) VALUES ("Katnis", "I volunteer.", "human");
-INSERT INTO characters (name, motto, species) VALUES ("Katnis", "I volunteer.", "human");
+INSERT INTO characters (name, motto, species) VALUES ("Takeo", "I am a ninja.", "human");
 INSERT INTO characters (name, motto, species) VALUES ("Katnis", "I volunteer.", "human");
 INSERT INTO characters (name, motto, species) VALUES ("Katnis", "I volunteer.", "human");
 
