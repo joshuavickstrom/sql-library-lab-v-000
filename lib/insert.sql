6874feeca1a7b9c3040b1f1bf248b9b1dcd158f4
Joshua Vickstrom <joshuavickstrom@outlook.com>
2018-02-20 17:26:35 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -4,4 +4,8 @@ INSERT INTO series (title) VALUES ("Tales of the Otori");
 INSERT INTO books (title, year) VALUES ("The Hunger Games", 2008), ("Catching Fire", 2009), ("Mocking Jay", 2010);
 INSERT INTO books (title, year) VALUES ("Across The Nightingale Floor", 2002), ("Grass For His Pillow", 2003), ("Brilliance Of The Moon", 2004);
 
-INSERT INTO characters, character_books () VALUES ();
+INSERT INTO characters (name, motto, species), character_books () VALUES ("Katnis", "I volunteer.", "human"), ();
+
+
+INSERT INTO authors (name) VALUES ("Suzanne Collins");
+INSERT INTO authors (name) VALUES ("Lian Hearn");
