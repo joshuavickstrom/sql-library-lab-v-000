d1f174bbf536929098b1db11bbc5982bc5bb09eb
Joshua Vickstrom <joshuavickstrom@outlook.com>
2018-02-20 17:14:47 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,2 +1,4 @@
 INSERT INTO series (title) VALUES ("Hunger Games");
 INSERT INTO series (title) VALUES ("Tales of the Otori");
+
+INSERT INTO books (title, year) VALUES ("The Hunger Games", 2008), ("Catching Fire", 2009), ("Mocking Jay", 2010);
