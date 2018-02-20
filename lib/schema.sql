8d2101fd175ab770e56d969b6fcfad40b67b4b47
Joshua Vickstrom <joshuavickstrom@outlook.com>
2018-02-20 16:59:41 +0000

    automatically backed up by learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -3,3 +3,13 @@ CREATE TABLE series (
   title TEXT,
   author_id INTEGER,
   subgenre_id INTEGER);
+
+  CREATE TABLE subgenre (
+    id INTEGER PRIMARY KEY,
+    name TEXT
+  );
+
+  CREATE TABLE authors (
+    id INTEGER PRIMARY KEY,
+    name TEXT
+  );
